e7da804f63dffd7607678e3cbea25fd1aa573a44
Sebastian Strube <sebstrfra@gmail.com>
2021-05-18 16:55:15 +0200

    First Steps

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |art|
+  art = Article.new(
+    title: Faker::Music::RockBand.name,
+    content: Faker::Quote.famous_last_words + " " + Faker::Quotes::Shakespeare.hamlet_quote + " " + Faker::TvShows::HowIMetYourMother.quote
+  )
+  art.save
+end
